29232d7fa619d69ab1a1bf330c99fa01c2e7625f
AJ Armstrong <aja@nait.ca>
2014-01-13 14:44:50 -0700

    First demo complete.
    Writeline demo.

---
+++ Demos/Demo 1/Demo 1/MyFirstInstructions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo_1
+{
+    class MyFirstInstructions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world.");
+            Console.WriteLine(); //Blank line
+
+            //Pause and wait for user.
+            Console.Write("Press Enter to Continue:"); //Just like writeline, but no new line.
+            Console.ReadLine();
+        }
+    }
+}
