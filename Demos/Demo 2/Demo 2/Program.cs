50acd05a19c623ad0813236081c11815637d4814
AJ Armstrong <aja@nait.ca>
2014-01-15 11:10:07 -0700

    Demos 2 and 3: String Formatting and Variables

---
+++ Demos/Demo 2/Demo 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+@"Windows IP Configuration
+
+She said ""You're really boring.""
+
+Ethernet adapter Local Area Connection:
+
+   Connection-specific DNS Suffix  . : nait.ca
+   Link-local IPv6 Address . . . . . : fe80::7de9:5aac:1dad:7e2a%11
+   IPv4 Address. . . . . . . . . . . : 10.129.254.110
+   Subnet Mask . . . . . . . . . . . : 255.255.254.0
+   Default Gateway . . . . . . . . . : 10.129.255.254
+
+Tunnel adapter Local Area Connection* 9:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :
+
+Tunnel adapter isatap.nait.ca:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . : nait.ca"
+   );
+            Console.WriteLine("\n\nSome blank lines\n\n");
+            Console.Write("\"It\'s a windy day\"\t\tYes, yes it is\nIs it murder if they're my own clones?");
+            Console.WriteLine(" This is on the same line.");
+            Console.Write("Aaaand this is on a different one.");
+
+            Console.Write("Press Enter to Continue:");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demos/Demo 3/Demo 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create some variables
+
+            int i = -3;
+            uint u = 42;
+            bool b = true;
+            char c = 'a';
+            string s = "foo";
+            double d = 2.71828182845904523536028747135266249775724709369995;
+
+            Console.WriteLine("i is equal to " + i);
+            Console.WriteLine("u is equal to " + u);
+            Console.WriteLine("b is equal to " + b);
+            Console.WriteLine("c is equal to '" + c + "'");
+            Console.WriteLine("s is equal to \"" + s + @"""");
+            Console.WriteLine("d is equal to " + d);
+
+            Console.WriteLine("The sum of i, u and d is " + (i + u + d));
+
+            Console.Write
+                        ("Press Enter to Continue:");
+            Console.ReadLine();
+
+        }
+    }
+}
