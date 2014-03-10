4a939f52b844e1df9f58cf6c07c7e493f696560e
AJ Armstrong <aja@nait.ca>
2014-03-10 13:27:23 -0600

    AJ's version of ICA10, from in-class walkthrough.

--- Demos/Demo 11/Demo 11/Program.cs
+++ Demos/Demo 11/Demo 11/Program.cs
@@ -11,17 +11,19 @@ namespace Demo_11
     class Program
     {
         static CDrawer Drawer = new CDrawer();
+        const int speed = 100; //Frames per second
 
         static void Main(string[] args)
         {
-            Drawer.Scale = 20;
-            uint speed = 1; //Frames per second
+            Random rand = new Random();
+            Drawer.Scale = 5;
 
-            for (int i = 1; i < 600/Drawer.Scale; ++i)
-            {
-                Thread.Sleep(1000/(int)speed);
-                MoveBrick(i - 1, i - 1, i, i, Color.DodgerBlue);
-            }
+            for (int i = 0; i < 100; ++i)
+                MoveBrickFromTo(rand.Next(800 / Drawer.Scale),
+                    rand.Next(600 / Drawer.Scale),
+                    rand.Next(800 / Drawer.Scale),
+                    rand.Next(600 / Drawer.Scale),
+                    RandColor.GetColor());
 
         }
 
@@ -31,5 +33,70 @@ namespace Demo_11
             Drawer.AddRectangle(xOld, yOld, 1, 1, Color.Black);
             Drawer.AddRectangle(xNew, yNew, 1, 1, color);
         }
+
+        static private void MoveBrickFromTo(int xOld, int yOld, int xNew, int yNew, Color color)
+        {
+            //Paint initial brick
+            int xCurr = xOld;
+            int yCurr = yOld;
+            Drawer.AddRectangle(xCurr, yCurr, 1, 1, color);
+
+            //Need to decide if I'm going left or right
+            int increment = 0;
+
+            if (xOld < xNew) //Moving right
+                increment = 1;
+            else if (xNew < xOld) //moving left
+                increment = -1;
+            else //moving vertically
+                increment = 0;
+
+            if (increment != 0) //Not vertical
+            {
+                //Figure out the line parameters to go from Old to New
+                int slope = Slope(xOld, yOld, xNew, yNew);
+                int intercept = Intercept(xOld, yOld, xNew, yNew);
+
+                for (int p = xOld; p != xNew; p = p + increment)
+                {
+                    //Pause for frame
+                    Thread.Sleep(1000 / speed);
+                    //Calculate new point (trig is fun!)
+                    int xNext = p;
+                    int yNext = slope * p + intercept;
+                    //Erase current, draw next one on x
+                    MoveBrick(xCurr, yCurr, xNext, yNext, color);
+                    //Reset current point for next iteration
+                    xCurr = xNext;
+                    yCurr = yNext;
+                }
+            }
+            else //need to paint vertically
+            {
+                //(yNew - yOld) / Math.Abs(yNew - yOld) will be +1 for going down, -1 for going up
+                for (int y = yOld; y != yNew; y = y + (yNew - yOld) / Math.Abs(yNew - yOld))
+                {
+                    Thread.Sleep(1000 / speed);
+                    MoveBrick(xNew, yCurr, xNew, y, color);
+                    yCurr = y;
+                }
+
+            }
+
+
+
+        }
+
+        static int Slope(int xO, int yO, int xN, int yN)
+        {
+            return (yN - yO) / (xN - xO);
+        }
+
+        static int Intercept(int xO, int yO, int xN, int yN)
+        {
+            int slope = Slope(xO, yO, xN, yN);
+            return yN - slope * xN;
+        }
+
     }
 }
---
+++ ICAs/ICA10/ICA10/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint numGrades = 0;
+            double grade = 0;
+            double weight = 0;
+            double sumNum = 0;
+            double sumDen = 0;
+            double average = 0;
+            bool goAgainP = false;
+            
+            DisplayTitle();
+            do
+            {
+                numGrades = GetUInt("Please enter the number of grades that you wish to average (max. 10): ", 1, 10);
+
+                for (int i = 1; i <= numGrades; ++i)
+                {
+                    grade = GetDouble(String.Format("Please enter the value for grade item #{0}: ", i),
+                            0, 100, "Grades must be between {0} and {1}");
+                    weight = GetDouble(String.Format("Please enter the weight of item #{0}: ", i),
+                            1, 50, "Grade item weights must be between {0} and {1}");
+                    sumNum += grade * weight;
+                    sumDen += weight;
+                }
+
+                average = sumNum / sumDen;
+                Console.WriteLine("\nYour weighted average for the supplied grades is {0:f2}",average);
+
+                goAgainP = AskYorN("Do you wish to perform another average?");
+            }
+            while (goAgainP);
+            Console.WriteLine("\nThank you for using the Weighted Average Calculator! See Ya!");
+           
+            PressAnyKey("Press any key to exit...");
+        }
+
+        static private double GetDouble(string prompt, double min, double max,
+            string errorMsg = "Please enter a number between {0} and {1}, inclusive.")
+        {
+            string input = null;
+            bool validp = false;
+            double val;
+
+            do
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine();
+                validp = double.TryParse(input, out val);
+                if (!validp)
+                    Console.Error.WriteLine("Invalid input.");
+                else if (val < min || val > max)
+                    Console.Error.WriteLine(errorMsg, min, max);
+            }
+            while (!validp || val < min || val > max);
+
+            return val;
+        }
+
+        static private uint GetUInt(string prompt, uint min, uint max)
+        {
+            string input = null;
+            bool validp = false;
+            uint val;
+
+            do
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine();
+                validp = uint.TryParse(input,out val);
+                if(!validp)
+                    Console.Error.WriteLine("Invalid input.");
+                else if(val < min || val > max)
+                    Console.Error.WriteLine("Please enter a number between {0} and {1}, inclusive.", min, max);
+            }
+            while(!validp || val < min || val > max);
+
+            return val;
+        }
+
+        static private void DisplayTitle(string title = "Welcome to the Weighted Grade Average Calculator!")
+        {
+            Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static private void PressAnyKey(string prompt = "Press any key to continue")
+        {
+            Console.Write(prompt);
+            Console.ReadKey(true);
+        }
+
+        static private bool AskYorN(string prompt = "Yes or no")
+        {
+            string response = null;
+            do
+            {
+                Console.Write(prompt);
+                response = Console.ReadLine();
+                response = response.ToLower();
+                if (response == "yes" || response == "y" || response == "ok" || response == "sure")
+                    return true;
+                if (response == "no" || response == "n" || response == "nope" || response == "negatory")
+                    return false;
+            }
+            while (true);
+        }
+    }
+}
