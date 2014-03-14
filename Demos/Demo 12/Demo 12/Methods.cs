85fa18575dcd50709a37edac6f6cebeae0ad8c9e
AJ Armstrong <aja@nait.ca>
2014-03-14 12:47:20 -0600

    Demos 12 and 13: Dlls and method

---
+++ Demos/Demo 12/Demo 12/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AJALibrary
+{
+    public class Methods
+    {
+        static public int Add (int x, int y = 1)
+        { return x + y;}
+        static public long Add(long x, long y = 1L)
+        { return x + y;}
+        static public double Add(double x, double y = 1.0)
+        { return x + y;}
+        static public float Add(float x, float y = 1.0f)
+        {  return x + y;}
+
+        static public void Pause(string message = "Press Any Key to Continue")
+       {
+            Console.Write(message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Demos/Demo 13/Demo 13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AJALibrary;
+
+namespace Demo_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Methods.Add(3,1.0));
+            Console.WriteLine(Methods.Add(3, 6));
+            Console.WriteLine(Methods.Add(1, 3.0));
+            Console.WriteLine(Methods.Add('a'));
+            Methods.Pause();
+        }
+    }
+}
